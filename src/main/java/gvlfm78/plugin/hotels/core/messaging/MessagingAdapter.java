b3aae0860a98f8f977884369933c60b3d64fb7c3
kernitus <1358227-kernitus@users.noreply.gitlab.com>
2019-06-19 14:28:27 +0200

    Added messaging adapters

---
+++ src/main/java/gvlfm78/plugin/hotels/core/messaging/MessagingAdapter.java
@@ -0,0 +1,61 @@
+/*
+ *     Hotels Bukkit and Sponge Plugin
+ *     Copyright (C) 2019 kernitus <kernitus@protonmail.com>
+ *     Full licence text can be found in LICENCE file
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package kernitus.plugin.hotels.core.messaging;
+
+import com.sk89q.worldguard.LocalPlayer;
+
+public interface MessagingAdapter {
+
+    /**
+     * Send a message to a player
+     * @param message Message to send
+     * @param player Player the message should be sent to
+     */
+    void send(String message, LocalPlayer player);
+
+    /**
+     * Prints a message in the console
+     * @param message Message to print
+     */
+    void print(String message);
+
+    /**
+     * Prints a debug message in the console, if debug is enabled
+     * @param message Message to print
+     */
+    default void debug(String message){
+        //TODO if debug enabled
+        print(MessagingAdapter.prependDebug(message));
+    }
+
+    /**
+     * Sends a debug message to a player, if debug is enabled
+     * @param message Message to send
+     * @param player Player the message should be sent to
+     */
+    default void debug(String message, LocalPlayer player){
+        //TODO if debug enabled
+        send(MessagingAdapter.prependDebug(message), player);
+    }
+
+    static String prependDebug(String message){
+        return "[DEBUG] " + message;
+    }
+}
---
+++ src/main/java/gvlfm78/plugin/hotels/sponge/SpongeMessagingAdapter.java
@@ -0,0 +1,38 @@
+/*
+ *     Hotels Bukkit and Sponge Plugin
+ *     Copyright (C) 2019 kernitus <kernitus@protonmail.com>
+ *     Full licence text can be found in LICENCE file
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package kernitus.plugin.hotels.sponge;
+
+import com.sk89q.worldedit.sponge.SpongeAdapter;
+import com.sk89q.worldguard.LocalPlayer;
+import kernitus.plugin.hotels.core.messaging.MessagingAdapter;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.channel.MessageChannel;
+
+public class SpongeMessagingAdapter implements MessagingAdapter {
+    @Override
+    public void send(String message, LocalPlayer player) {
+        MessageChannel.fixed(SpongeAdapter.adapt(player)).send(Text.of(message));
+    }
+
+    @Override
+    public void print(String message) {
+        System.out.println(message);
+    }
+}
